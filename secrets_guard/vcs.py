a8114135f67dd738368f3245d538f0a6f0ce0855
Docheinstein <dottorestefano97@gmail.com>
2019-10-27 00:12:15 +0200

    First commit

--- secrets_guard/vcs.py
+++ secrets_guard/vcs.py
@@ -12,8 +12,9 @@ def git_run(*args):
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             stdin=subprocess.PIPE,
-                            universal_newlines=True).wait(60)
-
+                            universal_newlines=True)
+    proc.wait(60)
+    stdout, stderr = proc.communicate()
 
 def push(local_path, remote_branch, commit_message):
     """
